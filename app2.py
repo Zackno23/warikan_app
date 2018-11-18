6b027c7981b00b48f92111f2b121d0d0468d4834
chikara <chikara.f.yoshida@gmail.com>
2018-11-18 10:44:34 +0900

    インポートされた変数名のリファクタリング

---
+++ app2.py
@@ -0,0 +1,13 @@
+# 割り勘計算 7000円 6人
+from warikan import calculate_warikan
+
+"""
+
+
+
+
+
+print(warikan(amount=1500, number_of_people=3))
+print(warikan(amount=2000, number_of_people=3))
+print(warikan(amount=3000, number_of_people=4))
+"""
